f5e699fbfa5e32bbb055cbbf8f09097814729b25
ROBRM91 <ramsel.medina1825@gmail.com>
2025-09-09 11:20:38 -0600

    connect.php

--- connect.php
+++ connect.php
@@ -1,37 +1,21 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recibe los datos del formulario POST
-    $servername = $_POST['server_name'];
-    $username = $_POST['user_name'];
-    $password = $_POST['password'];
-    $dbname = $_POST['db_name'];
-    $serverport = $_POST['server_port'];
+// Recibe los datos del formulario POST
+$servername = $_POST['server_name'];
+$username = $_POST['user_name'];
+$password = $_POST['password'];
+$dbname = $_POST['db_name'];
+$serverport = $_POST['server_port'];
 
-    // Crea una conexión usando MySQLi
-    $conn = new mysqli($servername, $username, $password, $dbname, $serverport);
+// Crea una conexión usando MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname, $serverport);
 
-    // Verifica si la conexión fue exitosa
-    if ($conn->connect_error) {
-        // Preparamos un mensaje de error
-        $error_message = "Error de conexión: " . $conn->connect_error;
-        
-        // Redirigimos de vuelta al formulario con el mensaje de error
-        // header("Location: index.html?error=" . urlencode($error_message));
-        echo "Conexión exitosa ", $dbname;
-        exit();
-    } else {
-        // Cerramos la conexión
-        $conn->close();
-        
-        // Redirige al usuario a la página de catálogos
-        // header("Location: iplanner-catalogos.html");
-        echo "Conexión exitosa ", $dbname;
-        exit();
-    }
+// Verifica si la conexión fue exitosa
+if ($conn->connect_error) {
+    die("❌ ¡Error de conexión!: " . $conn->connect_error);
 } else {
-    // Si no es una solicitud POST, redirigir al index
-    // header("Location: index.html?error=" . urlencode("Método no permitido"));
-    echo "Conexión exitosa ", $dbname;
-    exit();
+    // Redirige al usuario a la página de catálogos
+    echo "✅ ¡Conexión exitosa a la base de datos **" . $dbname . "**!";
+    header("Location: iplanner-catalogos.html");
+    exit(); // Es importante usar exit() después de una redirección
 }
 ?>
